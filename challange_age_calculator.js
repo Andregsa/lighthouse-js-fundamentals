0d49628bfc812917f643c134ecc0319a2062d868
Andre Galvao de Sa <andreg_sa@hotmail.com>
2021-11-01 04:23:53 +0000

    first commit

---
+++ challange_age_calculator.js
@@ -0,0 +1,9 @@
+const ageCalculator = (name, yearOfBirth, currentYear) => {
+  const age = currentYear - yearOfBirth;
+
+  return `${name} is ${age} years old.`;
+};
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+console.log(ageCalculator("Suzie", 1983, 2015)); //should return 32
