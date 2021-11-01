9e650cae166c8b2545321df970e2a8d013a63ab0
Andre Galvao de Sa <andreg_sa@hotmail.com>
2021-11-01 03:42:04 +0000

    first commit challange

---
+++ hello.js
@@ -0,0 +1,14 @@
+const sayHello = function () {
+  console.log("Hello, world");
+};
+
+sayHello();
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+};
+
+sayHello("Andre");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
